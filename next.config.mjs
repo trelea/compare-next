dc8893a5d411af729d20df11c501fdd432dfd962
trelea <treleamarius76@gmail.com>
2024-10-22 10:28:32 +0300

    new asf

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: 'dist',
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
