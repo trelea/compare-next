d160312db69bdcd93b5700fa98bee58923a3bd92
trelea <treleamarius76@gmail.com>
2024-10-23 14:34:45 +0300

    metatags

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,8 +5,21 @@ import '../globals.css';
 import { Loyout } from '@/components/_Loyout';
 
 export const metadata: Metadata = {
-  title: 'ComPare',
-  description: 'ComPare Best IT Solutions',
+  title: 'Dev Compare - Comprehensive IT Solutions and E-Commerce Services',
+  description:
+    'Dev Compare offers expert IT solutions, including development, web and mobile apps, e-commerce services, SEO, and quality assurance.',
+  keywords:
+    'IT solutions, development, web development, mobile apps, e-commerce, SEO, QA, software development',
+
+  viewport: 'width=device-width, initial-scale=1.0',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Dev Compare',
+    description:
+      'Transforming your digital presence with expert IT and e-commerce solutions.',
+    type: 'website',
+    url: 'https://www.devcompare.md',
+  },
 };
 
 export default async function LocaleLayout({
