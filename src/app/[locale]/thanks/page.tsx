f191f54f244df9f9bfa2650d53486127939df77f
trelea <treleamarius76@gmail.com>
2024-11-16 23:12:27 +0200

    quick fixes

--- src/app/[locale]/thanks/page.tsx
+++ src/app/[locale]/thanks/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 import { useTranslations } from 'next-intl';
 import React from 'react';
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: 'Thanks',
 };
 
